a8c3429677ef171d8fead02e9a65920e20583a33
lakshit k. singh <66665569+LakshitXD@users.noreply.github.com>
2021-07-09 00:40:45 +0530

    kaam chal ra 👀

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import "./App.css";
-import Login from "./Login.js";
-import React, { useState, useEffect } from "react";
-import { getTokenUrl } from "./spotify";
+import './App.css';
+import Login from './Login.js'
+import React,{useState,useEffect} from 'react';
+import { getTokenUrl } from './spotify';
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
-import { useDataLayerValue } from "./DataLayer";
+import { useDataLayerValue } from './DataLayer';
 
-const spotify = new SpotifyWebApi();
+const spotify =new SpotifyWebApi();
 
 function App() {
-  const [{ user, token }, dispatch] = useDataLayerValue();
+  const [{ user,token },dispatch] = useDataLayerValue();
 
-  useEffect(() => {
-    const hash = getTokenUrl();
-    window.location.hash = "";
-    const _token = hash.access_token;
-
-    if (_token) {
+  useEffect(()=>{
+    const hash=getTokenUrl();
+    window.location.hash="";
+    const _token =hash.access_token;
+    
+    if(_token){
       dispatch({
         type: "SET_TOKEN",
         token: _token,
-      });
+      })
       spotify.setAccessToken(_token);
-      spotify.getMe().then((user) => {
+      spotify.getMe().then((user) =>{
         dispatch({
           type: "SET_USER",
           user: user,
         });
+
+      });
+      spotify.getUserPlaylists().then((playlists)=>{
+        dispatch({
+          type:"SET_PLAYLISTS",
+          playlists: playlists,
+        });
       });
     }
-    console.log(_token);
-  }, []);
-  console.log(user);
+    
+  },[token,dispatch]);
 
-  return <div className="App">{token ? <Player /> : <Login />}</div>;
+  
+  return (
+    <div className="App">
+      {!token && <Login /> }
+      { token && <Player spotify/> }
+      
+    </div>
+  );
 }
 
 export default App;
--- src/DataLayer.js
+++ src/DataLayer.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { createContext, useContext, useReducer } from "react";
 
 export const DataLayerContext = createContext();
